976cdd6c3c53a191dac264cb549801c7eab88243
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-04-26 14:49:24 +0530

    todo.model

--- src/models/todo.model.ts
+++ src/models/todo.model.ts
@@ -25,12 +25,6 @@ export class Todo extends Entity {
   })
   isComplete?: boolean;
 
-  // Define well-known properties here
-
-  // Indexer property to allow additional data
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  // [prop: string]: any;
-
   constructor(data?: Partial<Todo>) {
     super(data);
   }
